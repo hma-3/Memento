32efc4b6a4ef1bb2fcce9301ebb8ec9cb977f9ce
hma-3 <maria.gula0307@gmail.com>
2023-12-17 12:00:59 +0200

    Add project files.

---
+++ Memento.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Memento {
+public:
+    Memento(const string& state) : state(state) {}
+
+    string getState() const {
+        return state;
+    }
+
+private:
+    string state;
+};
+
+class Originator {
+public:
+    void setState(const string& state) {
+        this->state = state;
+    }
+
+    Memento createMemento() const {
+        return Memento(state);
+    }
+
+    void restoreFromMemento(const Memento& memento) {
+        state = memento.getState();
+    }
+
+    void displayState() const {
+        cout << "Current State: " << state << endl;
+    }
+
+private:
+    string state;
+};
+
+int main() {
+    Originator originator;
+
+    originator.setState("State 1");
+    originator.displayState();
+
+    Memento snapshot = originator.createMemento();
+
+    originator.setState("State 2");
+    originator.displayState();
+
+    originator.restoreFromMemento(snapshot);
+    originator.displayState();
+
+    return 0;
+}
